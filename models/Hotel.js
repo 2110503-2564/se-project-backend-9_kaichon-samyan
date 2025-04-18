1348c439cb2adaf65e15a2e007e97491279b801b
Jirayut Najan <tonklaeiei123@gmail.com>
2025-04-18 16:27:36 +0700

    add hotel link pic

--- models/Hotel.js
+++ models/Hotel.js
@@ -1,47 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const HotelSchema = new mongoose.Schema({
   hotelName: {
     type: String,
-    require: [true, 'Please add hotel name']
+    require: [true, "Please add hotel name"],
   },
   address: {
     type: String,
-    required: [true, 'Please add an address']
+    required: [true, "Please add an address"],
   },
   website: {
-      type: String,
-      required: [true, 'Please add a website'],
-      unique: true
+    type: String,
+    required: [true, "Please add a website"],
+    unique: true,
   },
   description: {
-      type: String,
-      requried: [true, 'Please add a description']
+    type: String,
+    requried: [true, "Please add a description"],
+  },
+  picture: {
+    type: String,
+    default:
+      "https://img.freepik.com/free-vector/hotel-building-tropical-country-with-palms-cartoon-icon_1284-63176.jpg?t=st=1744968422~exp=1744972022~hmac=5255afad93b8d0b5e7fd4d5fd3d986ffe1101a5d8ce1bde333b563ff8abdf463&w=1800",
   },
   tel: {
     type: String,
-    required: [true, 'Please add a tel']
+    required: [true, "Please add a tel"],
   },
   rating: [
     {
       _id: {
         type: mongoose.Schema.Types.ObjectId,
-        default: () => new mongoose.Types.ObjectId()
+        default: () => new mongoose.Types.ObjectId(),
       },
       score: {
         type: Number,
-        required: true
+        required: true,
       },
       comment: {
         type: String,
       },
       user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-      }
-    }
-  ]
-})
+        ref: "User",
+        required: true,
+      },
+    },
+  ],
+});
 
-module.exports = mongoose.model('Hotel', HotelSchema);
+module.exports = mongoose.model("Hotel", HotelSchema);
